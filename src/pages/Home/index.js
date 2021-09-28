547e3e94667b2fc04d116dc5db3ac227fe79c3f4
Felipe Alves <felipeaamodio@gmail.com>
2021-09-28 20:54:16 -0300

    finalizado scroll dos filmes principais

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-import {Container, SearchContainer, SearchButton, Input} from './styles';
+import {ScrollView} from 'react-native';
+import {
+        Container, 
+        SearchContainer, 
+        SearchButton, 
+        Input, 
+        Title,
+        BannerButton,
+        Banner,
+        SliderMovie
+       } from './styles';
 
 import Header from '../../components/Header';
+import SliderItem from '../../components/SliderItem';
+
 import {Feather} from '@expo/vector-icons';
 
 export default function Home(){
@@ -19,6 +30,24 @@ export default function Home(){
                     <Feather name="search" size={30} color="#FFFFFF" />
                 </SearchButton>
             </SearchContainer>
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <Title>Em Cartaz</Title>
+
+                <BannerButton activeOpacity={0.9} onPress={() => alert('Banner')}>
+                    <Banner 
+                    source={{uri: 'https://images.unsplash.com/photo-1593642532842-98d0fd5ebc1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80'}}
+                    resizeMethod="resize"
+                    />
+                </BannerButton>
+
+                <SliderMovie 
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                    data={[1,2,3,4]}
+                    renderItem={({item}) => <SliderItem />}
+                />
+            </ScrollView>
         </Container>
     )
 }
